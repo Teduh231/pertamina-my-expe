a2ed09ab8347643b68d353393c48b8ee3e03f743
Virgoe <kakangtedduh@gmail.com>
2025-09-16 07:21:34 +0000

    ISINYA KAMU KEMANAIN

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -14,6 +14,7 @@ import {
   ShoppingCart,
   FileText,
   QrCode,
+  PanelLeft
 } from 'lucide-react';
 import {
   SidebarProvider,
@@ -61,6 +62,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
   ];
   
   if (!user) {
+    // For pages like login, signup, landing, we don't want the app layout
     return <>{children}</>;
   }
 
