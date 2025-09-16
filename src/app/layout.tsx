3c757274de59f996766e8b16847891c5590245f5
Rd Januar <ridwnv@gmail.com>
2025-09-16 08:57:04 +0000

    warna putih aih , kok gelap sih. Pikun lu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={`${poppins.variable} font-body antialiased bg-background text-foreground`}>
         <AuthProvider>
             {children}
