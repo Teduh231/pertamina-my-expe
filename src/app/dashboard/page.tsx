95d7f1f0be631f064651b576b2eceffc002fc619
Samuel Pandohan Terampil Gultom <sapategu@gmail.com>
2025-09-23 12:34:33 +0000

    make it same level with registration trends chart (_for element <Card>_)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -58,13 +58,9 @@ export default function DashboardPage() {
 
   return (
     <AppLayout>
-      <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-        <div className="lg:col-span-3 flex flex-col gap-6">
-          <AnalyticsDashboard booths={booths} attendees={attendees} />
-        </div>
-        <div className="lg:col-span-2">
-          <UpcomingEvents booths={booths} />
-        </div>
+      <div className="flex flex-col gap-6">
+         <AnalyticsDashboard booths={booths} attendees={attendees} />
+         <UpcomingEvents booths={booths} />
       </div>
     </AppLayout>
   );
